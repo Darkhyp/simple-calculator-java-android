59054c4154c12718f6bcb6731263e628a9ae346c
Darkhyp <a_v_kor73@rambler.ru>
2021-04-06 09:51:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# simple-calculator-java-android
+
+test of android studio 4:
+Implementing a Simple Calculator
+
+![alt text](portrait.PNG?raw=true)
+![alt text](https://github.com/Darkhyp/simple-calculator-java-android/blob/main/landscape.PNG?raw=true)
+
+
+# Author Alexander V. KOROVIN (a.v.korovin73@gmail.com)
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -15,10 +15,11 @@ import android.widget.Toast;
 
 import java.util.Locale;
 
+//public class MainActivity extends Activity {
 public class MainActivity extends AppCompatActivity {
     private String buffer = ""; // storage of the first number for mathematical calculations
     private char operation = ' '; // mathematical operation
-    private TextView textView = null; // TextView for number input
+    private TextView display = null; // TextView for number input
     private boolean isNewNumber = true;
     private char decimal_separator;
 
@@ -39,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
 
         // store variable when state is changed
-        outState.putString("displayed_number", textView.getText().toString());
+        outState.putString("displayed_number", display.getText().toString());
         outState.putBoolean("isNewNumber", isNewNumber);
         outState.putString("buffer", buffer);
         outState.putChar("operation", operation);
@@ -50,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
 
         // restore variable when state is changed
-        textView.setText(savedInstanceState.getString("displayed_number", ""));
+        display.setText(savedInstanceState.getString("displayed_number", ""));
         isNewNumber = savedInstanceState.getBoolean("isNewNumber", true);
         buffer = savedInstanceState.getString("buffer", "");
         operation = savedInstanceState.getChar("operation", ' ');
@@ -72,30 +73,48 @@ public class MainActivity extends AppCompatActivity {
             display.getSize(size);
             int width = size.x;
             int height = size.y;
-            textView.setText(String.format("%d x %d",width,height));
+            display.setText(String.format("%d x %d",width,height));
 */
-        textView = findViewById(R.id.dispaly);
+        display = findViewById(R.id.dispaly);
 
         // assign current decimal separator to the button 'buttonPoint'
         Button btn = findViewById(R.id.buttonPoint);
         btn.setText(Character.toString(decimal_separator));
 
         super.onStart();
+
     }
+
 
     public void addOperation(View v){
         /* add operation when the operation button was pressed */
         operation = ((Button)v).getText().toString().charAt(0);
         isNewNumber = true;
-        buffer = (String) textView.getText();
+        buffer = (String) display.getText();
+    }
+
+    public void changeSign(View v){
+        String currentNumber = display.getText().toString();
+
+        // change sign
+        if (currentNumber.charAt(0) == '-') {
+            currentNumber = currentNumber.substring(1);
+        }
+        else {
+            if (currentNumber.length() != 0) {
+                currentNumber = '-' + currentNumber;
+            }
+        }
+        display.setText(currentNumber);
     }
 
     public void addToNumber(View v){
         /* add a digit or decimal point or sign when the corresponding button was pressed */
-        String currentNumber,
-                pressedSymbol = ((Button)v).getText().toString();
+        String currentNumber;
+        String pressedSymbol = ((Button)v).getText().toString();
 
         if (isNewNumber) {
+            // if new number
             isNewNumber = false;
             if (pressedSymbol.charAt(0)==decimal_separator)
                 currentNumber = "0"+decimal_separator;
@@ -103,95 +122,104 @@ public class MainActivity extends AppCompatActivity {
                 currentNumber = pressedSymbol;
         }
         else{
-            currentNumber = textView.getText().toString();
-            if (pressedSymbol.charAt(0)=='±') {
-                // change sign
-                if (currentNumber.charAt(0) == '-')
-                    currentNumber = currentNumber.substring(1);
-                else
-                    currentNumber = pressedSymbol + currentNumber;
+            // add symbol to the number string
+            currentNumber = display.getText().toString();
+            // put decimal point
+            if (pressedSymbol.charAt(0)==decimal_separator) {
+                if (!currentNumber.contains(((Character) decimal_separator).toString()))
+                    currentNumber += decimal_separator;
             }
             else {
-                // put decimal point
-                if (pressedSymbol.charAt(0)==decimal_separator) {
-                    if (!currentNumber.contains(((Character) decimal_separator).toString()))
-                        currentNumber += decimal_separator;
-                }
-                else {
-                    currentNumber += pressedSymbol;
-                }
+                currentNumber += pressedSymbol;
             }
         }
-        textView.setText(currentNumber);
+        display.setText(currentNumber);
     }
 
     public void clearNumber(View v){
-        /* clear buffer and textView when the button 'Clear' was pressed */
+        /* clear buffer and display when the button 'Clear' was pressed */
         buffer = "";
         operation = ' ';
-        textView.setText("");
+        display.setText("");
     }
 
-    public boolean checkIfNumberInString(String buffer){
-        try {
-            float result = Float.parseFloat(buffer);
-        }
-        catch (Exception e){
-            return false;
-        }
-        return true;
-    }
-    public float getNumberFromString(@NonNull String buffer){
-        float result;
+    public String removeNonUsefullSymbols(@NonNull String buffer, boolean isBothSides){
+        int pos, ind_end;
+        // remove starting symbols
+        if (isBothSides)
+            while (true){
+                pos = buffer.indexOf(decimal_separator);
+                // remove decimal point at the end or zeros after decimal point
+                if(pos<0 & buffer.charAt(0)=='0' & buffer.length()>1) {
+                    // remove final symbol
+                    buffer = buffer.substring(1);
+                    continue;
+                }
+                break;
+            }
 
-        // check string
-        if(buffer.charAt(buffer.length()-1)==decimal_separator) {
-            buffer += '0';
+        // remove final symbols
+        while (true){
+            // remove decimal point at the end or zeros after decimal point
+            pos = buffer.indexOf(decimal_separator);
+            if(pos>=0){
+                // usefulness zeros at the end if decimal point is present
+                ind_end = buffer.length()-1;
+                if(buffer.charAt(ind_end)=='0') {
+                    // remove final symbol
+                    buffer = buffer.substring(0, ind_end);
+                }
+                ind_end = buffer.length()-1;
+                if(buffer.charAt(ind_end)==decimal_separator) {
+                    // remove final symbol
+                    buffer = buffer.substring(0, ind_end);
+                }
+                continue;
+            }
+            break;
         }
-
-        try {
-            result = Float.parseFloat(buffer);
-        }
-        catch (Exception e){
-            result = 0;
-        }
-        return result;
+        return buffer;
     }
 
     public void result(View v){
         /* perform an action when the button '=' was pressed */
 
         // read current number from buffer
-        float result = getNumberFromString(buffer);
-        // read current number from textView
-        float currentNumber = getNumberFromString(textView.getText().toString());
+        Float result = Float.parseFloat(removeNonUsefullSymbols(buffer, true));
+        // read current number from display
+        Float currentNumber = Float.parseFloat(removeNonUsefullSymbols(display.getText().toString(),true));
 
         isNewNumber = true;
 
         // check if buffer is correct
-        if (!checkIfNumberInString(buffer)) {
+        if (result == null) {
             operation = ' ';
         }
 
         // perform a mathematical operation on the contents of the "operation" attribute, or do nothing
-        switch (operation) {
-            case '+':
-                result += currentNumber;
-                break;
-            case '-':
-                result -= currentNumber;
-                break;
-            case '×':
-                result *= currentNumber;
-                break;
-            case '÷':
-                result /= currentNumber;
-                break;
-            default:
-                return;
+        try {
+            switch (operation) {
+                case '+':
+                    result += currentNumber;
+                    break;
+                case '-':
+                    result -= currentNumber;
+                    break;
+                case '×':
+                    result *= currentNumber;
+                    break;
+                case '÷':
+                    result /= currentNumber;
+                    break;
+                default:
+                    return;
+            }
         }
-        // put the result to the textView
-        textView.setText(Float.toString(result));
-//        textView.setText(String.format("%g", result));
+        catch (Exception e){
+            // pass
+        }
+        // put the result to the display
+        display.setText(removeNonUsefullSymbols(result.toString(), false));
+//        display.setText(String.format("%g", result));
     }
 }
